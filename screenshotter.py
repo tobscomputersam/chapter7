37754b12238873613d7c9d96fd8f02e68f5fadb1
tobscomputersam <ravankauw@gmail.com>
2018-04-19 14:19:20 -0500

    Create screenshotter.py
    Screenshotter

---
+++ screenshotter.py
@@ -0,0 +1,19 @@
+import win64gui
+import win64ui
+import win64con
+import win64api
+
+hdesktop = win32gui.GetDesktopWindow()
+width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+desktopDC = win32gui.GetWindowDC(Desktop)
+img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+mem_dc = img_dc.CreateCompatibleDC()
+screenshot = win32ui.CreateBitmap()
+screenshot.createCompatibleBitmap()
+mem_dc.selectObject(screenshot)
+mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp)
